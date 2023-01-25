b92fc204880e748eb1d29c185c64cde96fd34c2e
Chico <niklasschmitz2000@web.de>
2023-01-25 12:37:59 +0100

    Sensors implemented

--- car.js
+++ car.js
@@ -12,10 +12,12 @@ class Car {
         this.angle = 0
 
         this.controls = new Controls()
+        this.sensor = new Sensor(this)
     }
 
-    update() {
+    update(roadBorders) {
         this.#move()
+        this.sensor.update(roadBorders)
     }
 
     #move() {
@@ -65,5 +67,7 @@ class Car {
         ctx.fill()
 
         ctx.restore()
+
+        this.sensor.draw(ctx)
     }
 }
